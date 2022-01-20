b1502779f568e2101a83c3fd8135971d5339567d
Anatoliy Ostapenko <anatoliy.ostapenko@gmail.com>
2022-01-20 22:35:52 +0200

    Set video action

--- QRTestCode/ViewController.swift
+++ QRTestCode/ViewController.swift
@@ -6,14 +6,54 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
+    
+    var video = AVCaptureVideoPreviewLayer()
+    let session = AVCaptureSession() // Set session
+    
+    @IBOutlet weak var startVideoButton: UIButton!
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        startVideoButton.layer.cornerRadius = 25.0
+        
+        setupVideo()
+    }
+    
+    func setupVideo() {
+            
+        // Set video device
+        guard let captureDevice = AVCaptureDevice.default(for: AVMediaType.video) else { return print("error with capture device") }
+        
+        // Set input
+        do {
+            let input = try AVCaptureDeviceInput(device: captureDevice)
+            session.addInput(input)
+        } catch { fatalError(debugDescription) }
+        
+        // Set output
+        let output = AVCaptureMetadataOutput()
+        session.addOutput(output)
+        
+        // set video
+        video = AVCaptureVideoPreviewLayer(session: session)
+        video.frame = view.layer.bounds // set view bounds up to total screen
+    }
+    
+    
+    func startVideo() {
+        view.layer.addSublayer(video)
+        session.startRunning()
+        
     }
 
-
+    @IBAction func startVideoButtonPressed(_ sender: UIButton) {
+        
+        startVideo()
+    }
+    
 }
 
