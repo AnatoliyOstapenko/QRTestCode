c5cf9392135f3d6ae59c8904ec15067611771f00
Anatoliy Ostapenko <anatoliy.ostapenko@gmail.com>
2022-01-20 23:22:01 +0200

    QR Code mode implemented

--- QRTestCode/ViewController.swift
+++ QRTestCode/ViewController.swift
@@ -63,7 +63,42 @@ class ViewController: UIViewController {
 
 // MARK:- AVCaptureMetadataOutputObjectsDelegate Reading QR Code Method
 extension ViewController: AVCaptureMetadataOutputObjectsDelegate {
+    
+    // it happens when QR code detected
     func metadataOutput(_ output: AVCaptureMetadataOutput, didOutput metadataObjects: [AVMetadataObject], from connection: AVCaptureConnection) {
+        
+        print("metadataObjects = \(metadataObjects.count)")
+        
+        guard metadataObjects.count > 0 else { return }
+        
+        if let object = metadataObjects.first as? AVMetadataMachineReadableCodeObject {
+            if object.type == AVMetadataObject.ObjectType.qr {
+                
+                guard let urlString  = object.stringValue else { return }
+                
+                let alert = UIAlertController(title: "QR Code", message: object.stringValue, preferredStyle: .alert)
+                let goTo = UIAlertAction(title: "Go to...", style: .default) { (action) in
+                    print("object.stringValue = \(urlString)") // url string
+                }
+                let copy = UIAlertAction(title: "Copy...", style: .default) { (action) in
+                    
+                    // Copy url string to use everywhere
+                    UIPasteboard.general.string = object.stringValue
+                    
+                    // escape from video screen
+                    self.view.layer.sublayers?.removeLast()
+                    
+                    // stop session
+                    self.session.stopRunning()
+                }
+                alert.addAction(goTo)
+                alert.addAction(copy)
+                
+                present(alert, animated: true, completion: nil)
+                
+            }
+        }
+        
     
     }
 }
